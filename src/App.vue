cb1d1d7418a3f5f6eab9af82ae7ec448be4e0b40
bekalimuminov <bekalimuminov@gmail.com>
2025-12-25 23:38:52 +0500

    vue-practes

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+   import {ref} from "vue";
+
+   let count = ref(0);
+   function add (){
+     if (count.value < 10){
+       count.value++;
+     }
+   }
+   function remove (){
+     if (count.value > 0){
+       count.value--;
+     }
+   }
+   let key = ref("")
+</script>
+
+<template>
+<div :class="key === 'uchir' ? 'hidden' : ''">
+  <button @click="remove">-</button>
+  <input type="number" v-model="count">
+  <button @click="add">+</button>
+</div>
+  <input class="border border-red-500 rounded-[5px] py-[5px] px-[5px]" placeholder="yashirin suz kirit" type="text" v-model="key">
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/project.vue
@@ -0,0 +1,12 @@
+<template>
+<p>fgbhn</p>
+</template>
+
+<script setup>
+   async function data (){
+     let res = await fetch("http://localhost:3000/api/testREST")
+     let data = await res.json()
+     console.log(data)
+   }
+   data()
+</script>
