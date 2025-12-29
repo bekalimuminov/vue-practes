9e97b19dabd2d74eed967913d6b3b4ff4c4b54ce
bekalimuminov <bekalimuminov@gmail.com>
2025-12-29 13:09:38 +0500

    homework project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,73 @@
 <script setup>
-   import {ref} from "vue";
 
-   let count = ref(0);
-   function add (){
-     if (count.value < 10){
-       count.value++;
-     }
-   }
-   function remove (){
-     if (count.value > 0){
-       count.value--;
-     }
-   }
-   let key = ref("")
+import JordanJob from "./components/jobs/JordanJob.vue";
+import OgabekJob from "./components/jobs/OgabekJob.vue";
+import {computed, ref, watch} from "vue";
+
+
+let count = ref(0);
+
+watch(count, (newValue, oldValue) => {
+  console.log(`Eski qiymat: ${oldValue}`)
+  console.log(`Yangi qiymat: ${newValue}`)
+
+  if (count.value >= 10) {
+    alert("limitga yetdingiz")
+    return count.value = 10;
+  }else if (count.value <= 0) {
+    alert("limit tugadi")
+    return count.value = 0;
+  }
+})
+
+function add (){
+  count.value++
+}
+
+function remove (){
+  count.value--;
+}
+
+let name = ref('')
+let sureName = ref('')
+
+let fish = computed(() => {
+  return name.value + ' ' + sureName.value
+})
+
+let isTrue = ref(true)
+let div = ref('')
+
+watch(div, () => {
+  if (div.value === 'uchir') {
+    isTrue.value = false
+  }else {
+    isTrue.value = true
+  }
+})
 </script>
 
 <template>
-<div :class="key === 'uchir' ? 'hidden' : ''">
-  <button @click="remove">-</button>
-  <input type="number" v-model="count">
-  <button @click="add">+</button>
-</div>
-  <input class="border border-red-500 rounded-[5px] py-[5px] px-[5px]" placeholder="yashirin suz kirit" type="text" v-model="key">
+  <JordanJob name="Jovohir" job="gamer"/>
+  <OgabekJob name="Ogabek" job="mexanick"/>
+
+
+  <div>
+    <div>
+      <button @click="add">+</button>
+      <span>{{count}}</span>
+      <input v-if="isTrue" type="text" v-model="div" placeholder="kalit suz kirit">
+      <button @click="remove">-</button>
+    </div>
+  </div>
+
+
+  <p class="text-white">{{fish}}</p>
+
+    <input class="mr-[20px]" type="text" v-model="name">
+    <input type="text" v-model="sureName">
 </template>
+
 
 <style scoped>
 
---
+++ src/components/jobs/OgabekJob.vue
@@ -0,0 +1,11 @@
+<template>
+  <p>{{props.name}}</p>
+  <p>{{props.job}}</p>
+  <p></p>
+</template>
+<script setup>
+const props = defineProps({
+  name: '',
+  job: ''
+})
+</script>
